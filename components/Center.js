ade69f7a93d32e190f4575f761a46c30244f0037
BicksonNing <bicksonmeitei2@gmail.com>
2021-11-29 19:35:39 +0530

    initial

---
+++ components/Center.js
@@ -0,0 +1,9 @@
+function Center() {
+  return (
+    <div>
+      <h1>I am Center</h1>
+    </div>
+  );
+}
+
+export default Center;
---
+++ components/Player.js
@@ -0,0 +1,9 @@
+function Player() {
+  return (
+    <div className='h-20 bg-gray-500'>
+      <h1>I am Player</h1>
+    </div>
+  );
+}
+
+export default Player;
